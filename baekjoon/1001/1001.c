ce569c3bda874250cab7b05cc1f78b7451d13c2b
홍종민 <0723jongmin@naver.com>
2024-03-25 20:48:08 +0900

    bakejoon 2557

---
+++ baekjoon/1001/1001.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void){
+  
+
+
+ printf("Hello world");
+     printf("Hello world");
+  printf("Hello world");
+   printf("Hello world");
+
+
+
+return 0;
+
+    
+}
---
+++ baekjoon/1001/README.md
@@ -0,0 +1,3 @@
+# Algorithm
+* 조선대학교 HackerLogin 홍종민
+* 신입생 교육 과제 제출용 Repository
