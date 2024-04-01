799847e2e1cb9223ac9069f9ee83332242143934
홍종민 <0723jongmin@naver.com>
2024-04-01 20:30:50 +0900

    123123

--- baekjoon/1001/1001.c
+++ baekjoon/1001/1001.c
@@ -1,16 +1,5 @@
 #include <stdio.h>
 int main(void){
- int num;
- int sum=0;
-
- do{
-  printf("숫자를 입력하세요:");
-  scanf("%d", &num);
-  sum += num;
- }while(num !=0);
- printf("입력한 숫자들의 합은 %d입니다", sum);
 
  
-
-  return 0;
 }
